0531e4140afe4ff0cb12fd7aacc0b9974602ff4a
maungswan <swan_tayza_aung_htet_myet_kyaw@brown.edu>
2026-03-29 05:32:10 +0000

    aslkdfj

--- aut_app/wsgi.py
+++ aut_app/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aut_app.settings')
+os.environ['ANTHROPIC_API_KEY'] = 'your-key-here'
 
 application = get_wsgi_application()
+
